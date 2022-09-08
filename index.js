aba86f0f7843912105054b906a0e0f135c0d7a81
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-08 16:57:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Bài 1
+
+// let i;
+// let text = ''
+// for (i = 0; i < 5; i++) {
+//     text += 'The number is ' + i + '<br>';
+// }
+//
+// document.write(text);
+
+// Bài 2
+
+// let i;
+// let num = prompt('Nhập giá trị lớn hơn 0');
+// let total = 0;
+// for (i = 1; i <= num; i++) {
+//     total += i;
+// }
+//
+// alert(total);
+
+
+
+
